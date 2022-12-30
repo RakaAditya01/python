51c00f8115865a790da202cc43dc1217c82b3182
RakaAaditya01 <raka27275@gmail.com>
2022-12-30 15:57:10 +0700

    iseng

--- Bunga/dahlia.py
+++ Bunga/dahlia.py
@@ -4,7 +4,7 @@ turtle.bgcolor('black')
 turtle.speed(80)
 turtle.hideturtle()
 
-colors = ["white", "salmon", "pink", "red"]
+colors = ["#3C8FE8", "#3C33F5", "#3662FF", "#36CCFF"]
 
 for i in range(120):
     for c in colors:
